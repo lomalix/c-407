f902ee2a0c187b229da27a589d48c6e2bcc40e69
lomalix <1lomalix1@gmail.com>
2025-02-11 14:36:39 +0300

    pr9

---
+++ пр9/пр9/пр9.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "введите число";
+    while (0 < 1) {
+        int wh = 1;
+        int num;
+        cin >> num;
+        if (num <= 15 && num > 0) {
+            while (num + 1 > wh) {
+                cout << "цикл отработал" << wh << "круг" << endl;
+                wh++;
+            }
+        }
+        else if (num > 15 || num < 0) {
+            cout << "введите число от 1 до 15";
+        }
+        else if (num == 0)
+            break;
+    }
+    return 0;
+}
