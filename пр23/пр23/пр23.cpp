efcc580d7f536a460e64fbabd32be17b71082c68
lomalix <1lomalix1@gmail.com>
2025-02-11 14:45:38 +0300

    pr23

---
+++ пр23/пр23/пр23.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int mass[5];
+	for (int i = 0; i < 5; i++) {
+		cout << "введите число" << i + 1 << ":";
+		cin >> mass[i];
+	}
+	for (int q = 0; q < 5; q++) {
+		cout << "| таблица: " << q + 1 << endl << "-------------------------------\n| Чсло   | пример  | результат|" << endl << "-------------------------------\n|  " << mass[q] << "     |   " << mass[q] << "+" << mass[q] << "   |    "<<mass[q] + mass[q]<<"     | "<< endl << "-------------------------------\n|  " << mass[q] << "     |   " << mass[q] << "-" << mass[q] << "   |    " << mass[q] - mass[q] << "     | " << endl << "-------------------------------\n|  " << mass[q] << "     |   " << mass[q] << "/" << mass[q] << "   |    " << mass[q] / mass[q] << "     | " << endl << "-------------------------------\n|  " << mass[q] << "     |   " << mass[q] << "*" << mass[q] << "   |    " << mass[q] * mass[q] << "     | " << endl << endl << endl;
+	}
+	return 0;
+}
