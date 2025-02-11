c749c2cde81f1c3dbc5ffd940110d7f58f56ccf3
lomalix <1lomalix1@gmail.com>
2025-02-11 14:29:42 +0300

    pr2

---
+++ пр2/пр2/пр2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << " [1] лето\n [2] осень\n [3] зима\n [4] весна\n";
+	short num;
+	cout << "   введите время года:";
+	cin >> num;
+	if (num == 1) {
+		cout << "  время года лето, оденьте шорты и кепку";
+	}
+	else if (num == 2) {
+		cout << "  время года осень, не забудьте дождевик!";
+	}
+	else if (num == 3) {
+		cout << "  время года зима, оденьтесь потеплее";
+	}
+	else if (num == 4) {
+		cout << "  время года весна, радуйтесь жизни";
+	}
+	else {
+		cout << "  введите коректное число";
+	}
+	return 0;
+}
